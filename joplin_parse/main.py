70362fe25c109406b3efb8ffca8486167125022d
Rasul Kireev <me@rasulkireev.com>
2020-08-27 13:18:03 -0400

    Fix black and lint issues

--- joplin_parse/main.py
+++ joplin_parse/main.py
@@ -1,28 +1,23 @@
 import os
-import re
-import json
 import asyncio
 import argparse
 from joplin_api import JoplinApi
 from joplin_parse.utils import (
-    get_folder_names, 
-    find_position_in_list, 
     remove_spaces,
     generate_dict_with_folder_and_ids,
     generate_dict_with_all_notes_and_ids,
     generate_dict_with_all_resources,
     search_and_replace_joplin_note_links,
     search_and_replace_joplin_resource_links,
-    has_children,
     get_folder_title,
     download_resource,
     choose_folders_to_parse,
     generate_note,
-    generate_dict_with_all_resources_filenames
+    generate_dict_with_all_resources_filenames,
 )
 
-TOKEN ="dd17a885a394a1c18f66a23a5578cde0200f4177bca0eb7ad8cb1b60a25b402e48942740e59a8522222b918c8d202e9e871e22992d18ad3df8ec6ad6d13e8c7c"
 current_dir = os.getcwd()
+
 
 def parse_options():
     parser = argparse.ArgumentParser(description="Market Toolkit pdf report generator.")
@@ -36,15 +31,16 @@ def parse_options():
     )
 
     return parser.parse_args()
-    
+
+
 async def main(options):
     joplin = JoplinApi(token=options.joplin_token)
 
-    all_notes = (await joplin.get_notes()).json()    
+    all_notes = (await joplin.get_notes()).json()
     all_folders = (await joplin.get_folders()).json()
     all_resources = (await joplin.get_resources()).json()
-    notes_folder = 'notes'
-    resource_folder = 'resources'
+    notes_folder = "notes"
+    resource_folder = "resources"
 
     response = input("Do you want to parse all folders? [y/n]")
     if response == "n":
@@ -56,33 +52,40 @@ async def main(options):
 
     notes_dict = generate_dict_with_all_notes_and_ids(all_notes)
     folders_dict = generate_dict_with_folder_and_ids(all_folders)
-    
+
     resources_types_dict = generate_dict_with_all_resources(all_resources)
     resources_names_dict = generate_dict_with_all_resources_filenames(all_resources)
 
-
     for resource in all_resources:
-        response = await download_resource(joplin, resource, os.path.join(notes_folder, resource_folder))
+        response = await download_resource(
+            joplin, resource, os.path.join(notes_folder, resource_folder)
+        )
 
     if not os.path.exists(notes_folder):
         os.makedirs(notes_folder)
 
     for note in all_notes:
-        file_path = os.path.join(notes_folder, remove_spaces(note['title']))
+        file_path = os.path.join(notes_folder, remove_spaces(note["title"]))
         try:
-            note['body'] = search_and_replace_joplin_note_links(note['body'], notes_dict)
+            note["body"] = search_and_replace_joplin_note_links(
+                note["body"], notes_dict
+            )
         except KeyError:
-            note['body'] = search_and_replace_joplin_resource_links(note['body'], resources_types_dict, resources_names_dict, resource_folder)
+            note["body"] = search_and_replace_joplin_resource_links(
+                note["body"],
+                resources_types_dict,
+                resources_names_dict,
+                resource_folder,
+            )
 
-        if note['parent_id'] in folders_dict:
-            note['category'] = get_folder_title(note, folders_dict)
+        if note["parent_id"] in folders_dict:
+            note["category"] = get_folder_title(note, folders_dict)
             generate_note(file_path, note)
         else:
             continue
 
+
 if __name__ == "__main__":
     cli_options = parse_options()
-    
-    asyncio.run(
-        main(cli_options)
-    )
+
+    asyncio.run(main(cli_options))
--- joplin_parse/utils.py
+++ joplin_parse/utils.py
@@ -1,69 +1,60 @@
 import os
 import re
-import asyncio
 from typing import List
-from joplin_api import JoplinApi
+
 
 async def download_resource(joplin, resource, resource_location):
-        downloaded_resource = await joplin.download_resources(resource['id'])
-        
-        if resource['file_extension'] == "":
-            save_path = os.path.join(resource_location, f"{resource['filename']}")
-        else:
-            save_path = os.path.join(resource_location, f"{resource['id']}.{resource['file_extension']}")
+    downloaded_resource = await joplin.download_resources(resource["id"])
 
-        if not os.path.exists(resource_location):
-            os.makedirs(resource_location)
+    if resource["file_extension"] == "":
+        save_path = os.path.join(resource_location, f"{resource['filename']}")
+    else:
+        save_path = os.path.join(
+            resource_location, f"{resource['id']}.{resource['file_extension']}"
+        )
 
-        with open(save_path, "wb") as f:
-            f.write(downloaded_resource.content)
+    if not os.path.exists(resource_location):
+        os.makedirs(resource_location)
 
-        return downloaded_resource
+    with open(save_path, "wb") as f:
+        f.write(downloaded_resource.content)
+
+    return downloaded_resource
+
 
 def get_folder_names(folders):
     folder_names = []
     for folder in folders:
-        folder_names.append(folder['title'])
+        folder_names.append(folder["title"])
 
     return folder_names
 
-def has_children(folder: dict) -> bool:
-    """Tells us whether a folder has a subfolder.
 
-    Args:
-        folder (dict): Folder object.
-
-    Returns:
-        bool: Boolean
-    """
-    try:
-        folder['children']
-        return True
-    except KeyError:
-        return False
-        
 def find_position_in_list(parsed_list, item_in_search):
-    return (parsed_list.index(item_in_search))
+    return parsed_list.index(item_in_search)
+
 
 def search_for_joplin_links(text):
-    internal_links = re.findall("\(:/.+\)", text)
+    internal_links = re.findall(r"\(:/.+\)", text)
 
     link_ids = []
     for link in internal_links:
-        note_id = re.search("(?<=\(:\/).+?(?=\))", link).group()
+        note_id = re.search(r"(?<=\(:\/).+?(?=\))", link).group()
         link_ids.append(note_id)
-    
+
     return link_ids
+
 
 def find_linked_note(notes, link):
     for note in notes:
-        if note['id'] == link:
+        if note["id"] == link:
             linked_note = note
-    
+
     return linked_note
 
+
 def remove_spaces(text: str) -> str:
-    """Replace every non-word character (\W) with a dash.
+    """Replace every non-word character with a dash.
     Convinient when you need to make a URL slug.
 
     Args:
@@ -72,9 +63,10 @@ def remove_spaces(text: str) -> str:
     Returns:
         str: [description]
     """
-    new_text = re.sub(r'[^\w]', '-', text)
+    new_text = re.sub(r"[^\w]", "-", text)
 
     return new_text
+
 
 def search_and_replace_joplin_note_links(text: str, notes_dict: dict) -> str:
     """Goes through a block of text and replaces all the internal links with valid syntax
@@ -85,23 +77,29 @@ def search_and_replace_joplin_note_links(text: str, notes_dict: dict) -> str:
 
     Args:
         text (str): Text you want to go through
-        notes_dict (dict): Dictionary of all the joplin notes 
+        notes_dict (dict): Dictionary of all the joplin notes
 
     Returns:
         str: Text with replaced links.
     """
-    internal_links = re.findall("\(:/.+\)", text)
-    
+    internal_links = re.findall(r"\(:/.+\)", text)
+
     for link_to_replace in internal_links:
-        note_id_to_replace = re.search("(?<=\(:\/).+?(?=\))", link_to_replace).group()
+        note_id_to_replace = re.search(r"(?<=\(:\/).+?(?=\))", link_to_replace).group()
         note_title_replaced = notes_dict[note_id_to_replace]
         new_link = remove_spaces(note_title_replaced)
-        
-        text = text.replace(link_to_replace, f"(./{new_link})")
-    
-    return text    
 
-def search_and_replace_joplin_resource_links(text: str, resources_types_dict: dict, resources_names_dict: dict, resource_location: str) -> str:
+        text = text.replace(link_to_replace, f"(./{new_link})")
+
+    return text
+
+
+def search_and_replace_joplin_resource_links(
+    text: str,
+    resources_types_dict: dict,
+    resources_names_dict: dict,
+    resource_location: str,
+) -> str:
     """Goes through a block of text and replaces all the internal links with valid syntax. For resources.
 
     Example:
@@ -117,63 +115,50 @@ def search_and_replace_joplin_resource_links(text: str, resources_types_dict: di
     Returns:
         str: Text with replaced links.
     """
-    internal_links = re.findall("\(:/.+\)", text)
-    
+    internal_links = re.findall(r"\(:/.+\)", text)
+
     for link_to_replace in internal_links:
-        resource_id = re.search("(?<=\(:\/).+?(?=\))", link_to_replace).group()        
+        resource_id = re.search(r"(?<=\(:\/).+?(?=\))", link_to_replace).group()
         if resources_types_dict[resource_id] == "":
-            text = text.replace(link_to_replace, f"(./{resource_location}/{resources_names_dict[resource_id]})")
+            text = text.replace(
+                link_to_replace,
+                f"(./{resource_location}/{resources_names_dict[resource_id]})",
+            )
         else:
-            text = text.replace(link_to_replace, f"(./{resource_location}/{resource_id}.{resources_types_dict[resource_id]})")
+            text = text.replace(
+                link_to_replace,
+                f"(./{resource_location}/{resource_id}.{resources_types_dict[resource_id]})",
+            )
 
-    
-    return text    
+    return text
 
-def generate_dict_with_folder_and_ids(folders):
-    folders_dict = {}
-
-    for folder in folders:
-        folders_dict[folder['id']] = folder['title']
-
-        if has_children(folder):
-            folders = folder['children'] 
-            for folder in folders:
-                folders_dict[folder['id']] = folder['title']
-
-                if has_children(folder):
-                    folders = folder['children'] 
-                    for folder in folders:
-                        folders_dict[folder['id']] = folder['title']
-
-                        if has_children(folder):
-                            folders = folder['children'] 
-                            for folder in folders:
-                                folders_dict[folder['id']] = folder['title']
-    return folders_dict
 
 def generate_dict_with_all_notes_and_ids(notes):
     notes_dict = {}
 
     for note in notes:
-        notes_dict[note['id']] = note['title']
+        notes_dict[note["id"]] = note["title"]
 
     return notes_dict
+
 
 def generate_dict_with_all_resources(resources: List) -> dict:
     resources_types_dict = {}
 
     for resource in resources:
-        resources_types_dict[resource['id']] = resource['file_extension']
+        resources_types_dict[resource["id"]] = resource["file_extension"]
 
     return resources_types_dict
+
 
 def generate_dict_with_all_resources_filenames(resources: List) -> dict:
     resources_names_dict = {}
 
     for resource in resources:
-        resources_names_dict[resource['id']] = resource['filename']
+        resources_names_dict[resource["id"]] = resource["filename"]
 
     return resources_names_dict
+
 
 def get_folder_title(note: dict, folders_dict: dict) -> str:
     """Get the name of the folder, where note is located
@@ -185,10 +170,11 @@ def get_folder_title(note: dict, folders_dict: dict) -> str:
     Returns:
         str: Name of the folder.
     """
-    folder_id = note['parent_id']
+    folder_id = note["parent_id"]
     folder_title = folders_dict[folder_id]
-    
+
     return folder_title
+
 
 def choose_folders_to_parse(folders):
     parent_folder_names = get_folder_names(folders)
@@ -200,8 +186,9 @@ def choose_folders_to_parse(folders):
 
     return child_folders
 
+
 def generate_note(file_path, note):
-    with open(f'{file_path}.md','wb') as md_note:
+    with open(f"{file_path}.md", "wb") as md_note:
         s = f"""---
 title: "{note['title'].replace('"',"'")}"
 category: {note['category']}
@@ -213,4 +200,3 @@ created_at: {note['created_time']}
 {note['body']}
     """
         md_note.write(s.encode("utf-8"))
-
